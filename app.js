35ea5418750453189451f4de8a4cc4fea46c411b
Daniel Rode <dan.rode@gmail.com>
2021-12-30 13:50:22 -0500

    Updated app.js with a couple paths

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var express = require('express');
 const app = express();
-const contextRoot = '/';
+const contextRoot = '/test';
 
 app.get(contextRoot, (req, res) => {
-  res.send('<H2>AWS Fargate Node.js test app\n</H2>');
+  res.send('<H2>AWS Fargate Node.js test app\n</H2><br>/ path');
+})
+
+app.get(contextRoot, (req, res) => {
+  res.send('<H2>AWS Fargate Node.js test app\n</H2><br>/test path');
 })
 
 // Return 200 OK for load balancer check
-app.get('/check', (req, res) => {
+app.get('/test/check', (req, res) => {
   res.status(200).end();
   s
 
